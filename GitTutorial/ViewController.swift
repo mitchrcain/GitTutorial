36096dba41eaf1c2acef4c5b31922bb873ce072d
Mitchell Cain <mitch.r.cain@gmail.com>
2018-10-26 06:09:13 -0500

    This is just a test

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -12,8 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print(message)
     }
+    
+    let message = "I am so sleepy"
 
 
 }
